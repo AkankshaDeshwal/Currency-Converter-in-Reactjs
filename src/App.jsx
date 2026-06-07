867b6d0790928eacf433f50dd9b9df5ab74d2b88
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-07 14:01:44 +0530

    feat:Create skeleton UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
+import InputBox from './components/InputBox'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-     <div>
-      <h1>Currency Converter</h1>
+     <div className='bg-gray-950 flex flex-col justify-center items-center gap-12 w-full h-dvh'>
+      <h1 className='text-3xl font-semibold text-white'>Currency Converter</h1>
+      
+        <InputBox topLabel='from' />
+      <InputBox topLabel='to'/>
+    
      </div>
     </>
   )
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,8 +1,15 @@
-const InputBox = () => {
+const InputBox = ({topLabel}) => {
     return ( 
-        <div>
-            
-        </div>
+        <div className="flex flex-col rounded-md bg-white text-black text-md p-4 ">
+            <div className="text-xs text-gray-500 border-b px-2 border-gray-200" >{topLabel}</div>
+            <div className="flex gap-4 text-md"><input type='number' value={0} className="flex-1 p-2 focus:outline-0" />
+            <select name="currency" id="currency" >
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes" selected>Mercedes</option>
+  <option value="audi">Audi</option>
+</select></div>
+                    </div>
      );
 }
  
